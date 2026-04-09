e0de4252b92f125b22f2466b0565dfd572311646
Tiago Meurer <tiagomeurer@gmail.com>
2026-04-09 19:30:19 -0300

    update

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -40,9 +40,9 @@ export const Login: React.FC = () => {
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Acesse sua conta
         </h2>
-        <p className="mt-2 text-center text-sm text-gray-600">
+        {/* <p className="mt-2 text-center text-sm text-gray-600">
           Ou <Link to="/register" className="font-medium text-brand hover:text-brand-dark">crie uma conta gratuitamente</Link>
-        </p>
+        </p> */}
       </div>
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
